c965111071830923c044f4e7348db01a6d83b2aa
mitchdz <mitch_dz@hotmail.com>
2020-02-05 12:06:52 -0700

    lab1/t4: add output commands
    Signed-off-by: mitchdz <mitch_dz@hotmail.com>

--- lab1/task2-6/t4/t4_commands.sh
+++ lab1/task2-6/t4/t4_commands.sh
@@ -1,38 +1,62 @@
+#!/usr/bin/bash
 KEY="00112233445566778889aabbccddeeff"
 IV="01020304050607081234123412341212"
-enc_prefix="openssl enc -aes-128-"
+ENC_PREF="openssl enc -aes-128-"
 
-mkdir -p cipher/
-mkdir -p plain/
+CIPHER_PREF="cipher"
+PLAIN_PREF="plain"
+mkdir -p ${CIPHER_PREF}/
+mkdir -p ${PLAIN_PREF}/
 
 #encrypting
-${enc_prefix}ecb -e -in f1.txt -out cipher/f1_ecb_cipher.txt -K ${KEY}
-${enc_prefix}ecb -e -in f2.txt -out cipher/f2_ecb_cipher.txt -K ${KEY}
-${enc_prefix}ecb -e -in f3.txt -out cipher/f3_ecb_cipher.txt -K ${KEY}
-${enc_prefix}cbc -e -in f1.txt -out cipher/f1_cbc_cipher.txt -K ${KEY} -iv ${IV}
-${enc_prefix}cbc -e -in f2.txt -out cipher/f2_cbc_cipher.txt -K ${KEY} -iv ${IV}
-${enc_prefix}cbc -e -in f3.txt -out cipher/f3_cbc_cipher.txt -K ${KEY} -iv ${IV}
-${enc_prefix}cfb -e -in f1.txt -out cipher/f1_cfb_cipher.txt -K ${KEY} -iv ${IV}
-${enc_prefix}cfb -e -in f2.txt -out cipher/f2_cfb_cipher.txt -K ${KEY} -iv ${IV}
-${enc_prefix}cfb -e -in f3.txt -out cipher/f3_cfb_cipher.txt -K ${KEY} -iv ${IV}
-${enc_prefix}ofb -e -in f1.txt -out cipher/f1_ofb_cipher.txt -K ${KEY} -iv ${IV}
-${enc_prefix}ofb -e -in f2.txt -out cipher/f2_ofb_cipher.txt -K ${KEY} -iv ${IV}
-${enc_prefix}ofb -e -in f3.txt -out cipher/f3_ofb_cipher.txt -K ${KEY} -iv ${IV}
+${ENC_PREF}ecb -e -in f1.txt -out ${CIPHER_PREF}/f1_ecb_cipher.txt -K ${KEY}
+${ENC_PREF}ecb -e -in f2.txt -out ${CIPHER_PREF}/f2_ecb_cipher.txt -K ${KEY}
+${ENC_PREF}ecb -e -in f3.txt -out ${CIPHER_PREF}/f3_ecb_cipher.txt -K ${KEY}
+${ENC_PREF}cbc -e -in f1.txt -out ${CIPHER_PREF}/f1_cbc_cipher.txt -K ${KEY} \
+    -iv ${IV}
+${ENC_PREF}cbc -e -in f2.txt -out ${CIPHER_PREF}/f2_cbc_cipher.txt -K ${KEY} \
+    -iv ${IV}
+${ENC_PREF}cbc -e -in f3.txt -out ${CIPHER_PREF}/f3_cbc_cipher.txt -K ${KEY} \
+    -iv ${IV}
+${ENC_PREF}cfb -e -in f1.txt -out ${CIPHER_PREF}/f1_cfb_cipher.txt -K ${KEY} \
+    -iv ${IV}
+${ENC_PREF}cfb -e -in f2.txt -out ${CIPHER_PREF}/f2_cfb_cipher.txt -K ${KEY} \
+    -iv ${IV}
+${ENC_PREF}cfb -e -in f3.txt -out ${CIPHER_PREF}/f3_cfb_cipher.txt -K ${KEY} \
+    -iv ${IV}
+${ENC_PREF}ofb -e -in f1.txt -out ${CIPHER_PREF}/f1_ofb_cipher.txt -K ${KEY} \
+    -iv ${IV}
+${ENC_PREF}ofb -e -in f2.txt -out ${CIPHER_PREF}/f2_ofb_cipher.txt -K ${KEY} \
+    -iv ${IV}
+${ENC_PREF}ofb -e -in f3.txt -out ${CIPHER_PREF}/f3_ofb_cipher.txt -K ${KEY} \
+    -iv ${IV}
 #decrypting
-${enc_prefix}ecb -d -nopad -in cipher/f1_ecb_cipher.txt -out plain/f1_ecb_plain.txt -K ${KEY}
-${enc_prefix}ecb -d -nopad -in cipher/f1_cbc_cipher.txt -out plain/f1_cbc_plain.txt -K ${KEY}
-${enc_prefix}cfb -d -nopad -in cipher/f1_cfb_cipher.txt -out plain/f1_cfb_plain.txt -K ${KEY} -iv ${IV}
-${enc_prefix}ofb -d -nopad -in cipher/f1_ofb_cipher.txt -out plain/f1_ofb_plain.txt -K ${KEY} -iv ${IV}
+${ENC_PREF}ecb -d -nopad -in ${CIPHER_PREF}/f1_ecb_cipher.txt \
+    -out ${PLAIN_PREF}/f1_ecb_plain.txt -K ${KEY}
+${ENC_PREF}ecb -d -nopad -in ${CIPHER_PREF}/f1_cbc_cipher.txt \
+    -out ${PLAIN_PREF}/f1_cbc_plain.txt -K ${KEY}
+${ENC_PREF}cfb -d -nopad -in ${CIPHER_PREF}/f1_cfb_cipher.txt \
+    -out ${PLAIN_PREF}/f1_cfb_plain.txt -K ${KEY} -iv ${IV}
+${ENC_PREF}ofb -d -nopad -in ${CIPHER_PREF}/f1_ofb_cipher.txt \
+    -out ${PLAIN_PREF}/f1_ofb_plain.txt -K ${KEY} -iv ${IV}
 
-${enc_prefix}ecb -d -nopad -in cipher/f2_ecb_cipher.txt -out plain/f2_ecb_plain.txt -K ${KEY}
-${enc_prefix}cbc -d -nopad -in cipher/f2_cbc_cipher.txt -out plain/f2_cbc_plain.txt -K ${KEY} -iv ${IV}
-${enc_prefix}cfb -d -nopad -in cipher/f2_cfb_cipher.txt -out plain/f2_cfb_plain.txt -K ${KEY} -iv ${IV}
-${enc_prefix}ofb -d -nopad -in cipher/f2_ofb_cipher.txt -out plain/f2_ofb_plain.txt -K ${KEY} -iv ${IV}
+${ENC_PREF}ecb -d -nopad -in ${CIPHER_PREF}/f2_ecb_cipher.txt \
+    -out ${PLAIN_PREF}/f2_ecb_plain.txt -K ${KEY}
+${ENC_PREF}cbc -d -nopad -in ${CIPHER_PREF}/f2_cbc_cipher.txt \
+    -out ${PLAIN_PREF}/f2_cbc_plain.txt -K ${KEY} -iv ${IV}
+${ENC_PREF}cfb -d -nopad -in ${CIPHER_PREF}/f2_cfb_cipher.txt \
+    -out ${PLAIN_PREF}/f2_cfb_plain.txt -K ${KEY} -iv ${IV}
+${ENC_PREF}ofb -d -nopad -in ${CIPHER_PREF}/f2_ofb_cipher.txt \
+    -out ${PLAIN_PREF}/f2_ofb_plain.txt -K ${KEY} -iv ${IV}
 
-${enc_prefix}ecb -d -nopad -in cipher/f3_ecb_cipher.txt -out plain/f3_ecb_plain.txt -K ${KEY}
-${enc_prefix}cbc -d -nopad -in cipher/f3_cbc_cipher.txt -out plain/f3_cbc_plain.txt -K ${KEY} -iv ${IV}
-${enc_prefix}cfb -d -nopad -in cipher/f3_cfb_cipher.txt -out plain/f3_cfb_plain.txt -K ${KEY} -iv ${IV}
-${enc_prefix}ofb -d -nopad -in cipher/f3_ofb_cipher.txt -out plain/f3_ofb_plain.txt -K ${KEY} -iv ${IV}
+${ENC_PREF}ecb -d -nopad -in ${CIPHER_PREF}/f3_ecb_cipher.txt \
+    -out ${PLAIN_PREF}/f3_ecb_plain.txt -K ${KEY}
+${ENC_PREF}cbc -d -nopad -in ${CIPHER_PREF}/f3_cbc_cipher.txt \
+    -out ${PLAIN_PREF}/f3_cbc_plain.txt -K ${KEY} -iv ${IV}
+${ENC_PREF}cfb -d -nopad -in ${CIPHER_PREF}/f3_cfb_cipher.txt \
+    -out ${PLAIN_PREF}/f3_cfb_plain.txt -K ${KEY} -iv ${IV}
+${ENC_PREF}ofb -d -nopad -in ${CIPHER_PREF}/f3_ofb_cipher.txt \
+    -out ${PLAIN_PREF}/f3_ofb_plain.txt -K ${KEY} -iv ${IV}
 
 #printing out results
 echo "f1.txt"
@@ -50,7 +74,6 @@ printf "cbc  : "; xxd plain/f2_cbc_plain.txt
 printf "cfb  : "; xxd plain/f2_cfb_plain.txt
 printf "ofb  : "; xxd plain/f2_ofb_plain.txt
 
-
 echo ""
 echo "f3.txt"
 printf "plain: "; xxd f3.txt
@@ -58,7 +81,3 @@ printf "ecb  : "; xxd plain/f3_ecb_plain.txt
 printf "cbc  : "; xxd plain/f3_cbc_plain.txt
 printf "cfb  : "; xxd plain/f3_cfb_plain.txt
 printf "ofb  : "; xxd plain/f3_ofb_plain.txt
-
-
-
-
