aac5d7ae0f9f1f8a935ade0249a50c174efb8e5d
mitchdz <mitch_dz@hotmail.com>
2020-03-04 18:08:03 -0500

    l3t3: finish
    Signed-off-by: mitchdz <mitch_dz@hotmail.com>

---
+++ lab3/t3/workspace/l3t3.sh
@@ -0,0 +1,11 @@
+head -c 4224 a.out > prefix
+md5collgen -p prefix -o P Q
+tail -c +4352 a.out > suffix
+cat P suffix > new1
+cat Q suffix > new2
+md5sum new1 new2
+chmod +x new1
+chmod +x new2
+echo $(./new1) | md5sum
+echo $(./new2) | md5sum
+
