c69330a9e1b3770a8aeeadfb4a4804c4746c7224
mitchell dzurick <mitch_dz@hotmail.com>
2020-03-04 20:01:11 -0700

    [WIP]

--- lab3/t4/l3t4.sh
+++ lab3/t4/l3t4.sh
@@ -1,12 +1,23 @@
-d -c 4224 a.out > prefix
-md5collgen -p prefix -o P Q
-tail -c +4352 a.out > suffix
-cat P suffix > new1
-cat Q suffix > new2
-md5sum new1 new2
-chmod +x new1
-chmod +x new2
-echo $(./new1) | md5sum
-echo $(./new2) | md5sum
+head -c 4224 a.out > prefix
+md5collgen -p prefix -o out1 out2
+tail -c +4353 a.out > suffixtest
+
+head -c 8 suffixtest > arrcomplete
+cat out1 arrcomplete > out1arrcomplete
+cat out2 arrcomplete > out2arrcomplete
+tail -c +9 suffixtest > suffix
+
+tail -c +25 suffix > suffixtest
+head -c 24 suffix > tillnext
+cat out1arrcomplete tillnext > file1tillnext
+cat out2arrcomplete tillnext > file2tillnext
+
+
+
+tail -c +201 suffixtest > suffix
+tail -c +4161 out1arrcomplete > comparray
+cat file1tillnext comparray suffix > firstexec
+cat file2tillnext comparray suffix > secondexec
+
 
 
