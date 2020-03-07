fd22e50577c9cdf4369fe716e0a3ba5106844688
mitchdz <mitch_dz@hotmail.com>
2020-03-06 23:26:13 -0500

    [WIP]

--- lab3/t4/l3t4.sh
+++ lab3/t4/l3t4.sh
@@ -1,6 +1,10 @@
-head -c 4224 a.out > prefix
+#START="16640"
+START="4192"
+END=$(($START + 129))
+
+head -c ${START} a.out > prefix
 md5collgen -p prefix -o out1 out2
-tail -c +4353 a.out > suffixtest
+tail -c +${END} a.out > suffixtest
 
 head -c 8 suffixtest > arrcomplete
 cat out1 arrcomplete > out1arrcomplete
@@ -12,12 +16,14 @@ head -c 24 suffix > tillnext
 cat out1arrcomplete tillnext > file1tillnext
 cat out2arrcomplete tillnext > file2tillnext
 
-
-
 tail -c +201 suffixtest > suffix
 tail -c +4161 out1arrcomplete > comparray
 cat file1tillnext comparray suffix > firstexec
 cat file2tillnext comparray suffix > secondexec
 
+md5sum firstexec secondexec
+chmod +x firstexec
+chmod +x secondexec
 
-
+./firstexec
+./secondexec
