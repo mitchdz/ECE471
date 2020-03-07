187771817b6fe8c79d37493dc7e23464f8cc1fde
mitchell dzurick <mitch_dz@hotmail.com>
2020-03-06 21:38:21 -0700

    [WIP]

--- lab3/t4/l3t4.sh
+++ lab3/t4/l3t4.sh
@@ -4,26 +4,26 @@ END=$(($START + 129))
 
 head -c ${START} a.out > prefix
 md5collgen -p prefix -o out1 out2
-tail -c +${END} a.out > suffixtest
+tail -c +${END} a.out > suffixtemp
 
-head -c 8 suffixtest > arrcomplete
-cat out1 arrcomplete > out1arrcomplete
-cat out2 arrcomplete > out2arrcomplete
-tail -c +9 suffixtest > suffix
+head -c 8 suffixtemp > arrtemp
+cat out1 arrtemp > out1arr
+cat out2 arrtemp > out2arr
+tail -c +9 suffixtemp > suffix
 
-tail -c +25 suffix > suffixtest
-head -c 24 suffix > tillnext
-cat out1arrcomplete tillnext > file1tillnext
-cat out2arrcomplete tillnext > file2tillnext
+tail -c +25 suffix > suffixtemp
+head -c 24 suffix > buffer
+cat out1arr buffer > file1buffer
+cat out2arr buffer > file2buffer
 
-tail -c +201 suffixtest > suffix
-tail -c +4161 out1arrcomplete > comparray
-cat file1tillnext comparray suffix > firstexec
-cat file2tillnext comparray suffix > secondexec
+tail -c +201 suffixtemp > suffix
+tail -c +4161 out1arr > comparray
+cat file1buffer comparray suffix > goodCode
+cat file2buffer comparray suffix > maliciousCode
 
-md5sum firstexec secondexec
-chmod +x firstexec
-chmod +x secondexec
+md5sum goodCode maliciousCode
+chmod +x goodCode
+chmod +x maliciousCode
 
-./firstexec
-./secondexec
+./goodCode
+./maliciousCode
